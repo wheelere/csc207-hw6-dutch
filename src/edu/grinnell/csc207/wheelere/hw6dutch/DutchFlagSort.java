aab8b3c0ab4b962570a619355cbe4dc87b943f1b
John Brady <bradyjoh@grinnell.edu>
2013-10-05 16:07:52 -0500

    How in the world did it end up in the bin?!

--- src/edu/grinnell/csc207/wheelere/hw6dutch/DutchFlagSort.java
+++ src/edu/grinnell/csc207/wheelere/hw6dutch/DutchFlagSort.java
@@ -4,42 +4,44 @@ import java.io.PrintWriter;
 
 public class DutchFlagSort {
 
-	public static void DutchSort(int[] array) throws Exception {
-		int len = array.length;
-		int r = 0;
-		int b = len - 1;
-		int i = 0;
-		int temp;
-		while(i <= b) {
-			if (array[i] == 1)  {
-				temp = array[r];
-				array[r] = array[i];
-				array[i] = temp;
-				r++;
-				i++;
-			}
-			else if (array[i] == 2) {
-				i++;
-			}
-			else if (array[i] == 3) {
-				temp = array[b];
-				array[b] = array[i];
-				array[i] = temp;
-				b--;
-			}
-			else {
-				throw new Exception("The element " + array[i] + "is invalid");
-			}
-		}
+    /*
+     * dutchSort takes an array of values 1, 2, and 3 (where 1=red, 2=white,
+     * 3=blue) and sorts it so the 1s come first, 2s come second, and 3s come
+     * third
+     */
+    public static void dutchSort(int[] array) throws Exception {
+	int len = array.length;
+	int r = 0; // Splitter index for red|white
+	int b = len - 1; // Splitter index for white|blue
+	int i = 0; // current index
+	int temp; // a holder for switching
+	while (i <= b) {
+	    if (array[i] == 1) {
+		temp = array[r];
+		array[r] = array[i];
+		array[i] = temp;
+		r++;
+		i++;
+	    } else if (array[i] == 2) {
+		i++;
+	    } else if (array[i] == 3) {
+		temp = array[b];
+		array[b] = array[i];
+		array[i] = temp;
+		b--;
+	    } else {
+		throw new Exception("The element " + array[i] + "is invalid");
+	    }
 	}
-	
-	public static void main(String[] args) throws Exception {
-		int[] array1 = {1, 2, 3, 1, 2, 3, 1, 3, 3, 2, 2, 2, 2, 2, 2, 1};
-		DutchSort(array1);
-		PrintWriter pen = new PrintWriter(System.out, true);
-		for(int i = 0; i < array1.length; i++) {
-			pen.print(array1[i] + " ");
-		}
-		pen.flush();
+    }
+
+    public static void main(String[] args) throws Exception {
+	int[] array1 = { 1, 2, 3, 1, 2, 3, 1, 3, 3, 2, 2, 2, 2, 2, 2, 1 };
+	dutchSort(array1);
+	PrintWriter pen = new PrintWriter(System.out, true);
+	for (int i = 0; i < array1.length; i++) {
+	    pen.print(array1[i] + " ");
 	}
+	pen.flush();
+    }
 }
